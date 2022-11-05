f26f76be30e08aa971cee33746706d40c6d2367b
Jordan Kaye <jordan.kaye2@gmail.com>
2022-11-05 07:45:56 -0600

    Locates curl with find_package
    This will be more robust for future linkage.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#include <curl/curl.h>
+#include <curl/easy.h>
+
 #include "include/http_client.hpp"
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  return 0;
+}
