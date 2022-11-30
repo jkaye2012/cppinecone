13e4654eb30b5c37af1ae5e47197179a70a30d13
Jordan Kaye <jordan.kaye2@gmail.com>
2022-11-30 07:31:54 -0700

    Fully integrates Doxygen infrastructure

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+set -eo pipefail
 
 SCRIPTPATH="$(dirname "$0")"
 cd "$SCRIPTPATH" || exit 255
 
-if [ "$1" == "--retag" ]; then
+SHOULD_RETAG=0
+
+while (("$#")); do
+    case "$1" in
+    -h | --help)
+        echo "Usage: $0 [options]"
+        echo "Options:"
+        echo "  -h, --help      Show this help message and exit."
+        echo "  -r, --retag     Rebuild and tag the Docker image locally."
+        ;;
+    -r | --retag)
+        SHOULD_RETAG=1
+        ;;
+    *) break ;;
+    esac
+    shift
+done
+
+if [ $SHOULD_RETAG -eq 1 ]; then
     docker build -t cppinecone-build .
 fi
 
-docker run --mount type=bind,source="$(pwd)",target=/app -p 8000:8000 -it cppinecone-build /app/mkdocs.sh
+# shellcheck disable=SC2068
+docker run --mount type=bind,source="$(pwd)",target=/app -p 8000:8000 -it cppinecone-build /app/mkdocs.sh $@
--- mkdocs.sh
+++ mkdocs.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+# TODO: set -eo pipefail
+
 SCRIPTPATH="$(dirname "$0")"
 cd "$SCRIPTPATH" || exit 255
 
-doxygen Doxyfile
+SHOULD_DOXYGEN=1
+
+while (("$#")); do
+    case "$1" in
+    -h | --help)
+        echo "Usage: $0 [options]"
+        echo "Options:"
+        echo "  -h, --help              Show this help message and exit."
+        echo "  -sd, --skip-doxygen     Do not generate Doxygen documentation."
+        ;;
+    -sd | --skip-doxygen)
+        SHOULD_DOXYGEN=0
+        ;;
+    *) break ;;
+    esac
+    shift
+done
+
+if [ $SHOULD_DOXYGEN -eq 1 ]; then
+    doxygen Doxyfile
+fi
 mkdocs serve -a 0.0.0.0:8000
