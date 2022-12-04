413c0113cdcc0292c4080c75e4a1e66ec13674b3
Jordan Kaye <jordan.kaye2@gmail.com>
2022-12-04 09:31:42 -0700

    Adds strong type for index metrics

--- include/pinecone/types/index_types.hpp
+++ include/pinecone/types/index_types.hpp
@@ -71,6 +71,41 @@ inline auto to_string(pod_size size) noexcept -> std::string
 }
 
 /**
+ * @brief Metric types for individual databases.
+ */
+enum class metric_type {
+  euclidean,
+  cosine,
+  dotproduct
+};
+
+inline auto to_string(metric_type type) noexcept -> std::string
+{
+  switch (type) {
+    case metric_type::euclidean:
+      return "euclidean";
+    case metric_type::cosine:
+      return "cosine";
+    case metric_type::dotproduct:
+      return "dotproduct";
+  }
+}
+
+inline auto parse_metric(std::string const& metric) -> util::result<metric_type>
+{
+  if (metric == "euclidean") {
+    return metric_type::euclidean;
+  }
+  if (metric == "cosine") {
+    return metric_type::cosine;
+  }
+  if (metric == "dotproduct") {
+    return metric_type::dotproduct;
+  }
+  return {"failed to parse metric type"};
+}
+
+/**
  * @brief Pod configuration for an individual index.
  */
 struct pod_configuration {
@@ -181,15 +216,13 @@ struct database_status {
   }
 };
 
-// TODO: metric could probably be made an enum
-
 /**
  * @brief The complete description of an individual database.
  */
 struct database {
   [[nodiscard]] constexpr auto name() const noexcept -> std::string const& { return _name; }
   [[nodiscard]] constexpr auto dimension() const noexcept -> uint32_t { return _dimension; }
-  [[nodiscard]] constexpr auto metric() const noexcept -> std::string const& { return _metric; }
+  [[nodiscard]] constexpr auto metric() const noexcept -> metric_type { return _metric; }
   [[nodiscard]] constexpr auto pod_type() const noexcept -> pod_configuration const&
   {
     return _pod_config;
@@ -203,29 +236,36 @@ struct database {
   {
     auto status = database_status::build(api_result["status"]);
     auto json_db = api_result["database"];
+
+    auto metric = parse_metric(json_db["metric"]);
+    if (metric.is_failed()) {
+      return metric.propagate<database>();
+    }
+
     auto pod_config = pod_configuration::build(json_db["pod_type"]);
     if (pod_config.is_failed()) {
       return pod_config.propagate<database>();
     }
-    return database(json_db["name"], json_db["dimension"], json_db["metric"], *pod_config,
-                    json_db["pods"], json_db["replicas"], json_db["shards"], status);
+
+    return database(json_db["name"], json_db["dimension"], *metric, *pod_config, json_db["pods"],
+                    json_db["replicas"], json_db["shards"], status);
   }
 
  private:
   std::string _name;
   uint32_t _dimension;
-  std::string _metric;
+  metric_type _metric;
   pod_configuration _pod_config;
   uint16_t _pods;
   uint16_t _replicas;
   uint16_t _shards;
   database_status _status;
 
-  database(std::string name, uint32_t dimension, std::string metric, pod_configuration pod_config,
+  database(std::string name, uint32_t dimension, metric_type metric, pod_configuration pod_config,
            uint16_t pods, uint16_t replicas, uint16_t shards, database_status status) noexcept
       : _name(std::move(name)),
         _dimension(dimension),
-        _metric(std::move(metric)),
+        _metric(metric),
         _pod_config(pod_config),
         _pods(pods),
         _replicas(replicas),
@@ -322,9 +362,9 @@ struct new_index {
     {
     }
 
-    auto with_metric(std::string metric) noexcept -> builder&
+    auto with_metric(metric_type metric) noexcept -> builder&
     {
-      _metric = std::move(metric);
+      _metric = metric;
       return *this;
     }
 
@@ -368,7 +408,7 @@ struct new_index {
     {
       return {std::move(_name),
               _dimension,
-              std::move(_metric),
+              _metric,
               _pods,
               _pod_config,
               _shards,
@@ -380,7 +420,7 @@ struct new_index {
    private:
     std::string _name;
     uint32_t _dimension;
-    std::optional<std::string> _metric;
+    std::optional<metric_type> _metric;
     std::optional<uint16_t> _pods;
     std::optional<pod_configuration> _pod_config;
     std::optional<uint16_t> _shards;
@@ -393,7 +433,7 @@ struct new_index {
   {
     json repr = {{"name", _name}, {"dimension", _dimension}};
     if (_metric) {
-      repr["metric"] = *_metric;
+      repr["metric"] = to_string(*_metric);
     }
     if (_pods) {
       repr["pods"] = *_pods;
@@ -420,7 +460,7 @@ struct new_index {
  private:
   std::string _name;
   uint16_t _dimension;
-  std::optional<std::string> _metric;  // TODO: could be made enum
+  std::optional<metric_type> _metric;
   std::optional<uint16_t> _pods;
   std::optional<pod_configuration> _pod_config;
   std::optional<uint16_t> _shards;
@@ -428,14 +468,14 @@ struct new_index {
   std::optional<std::vector<std::string>> _metadata_config;
   std::optional<std::string> _source_collection;
 
-  new_index(std::string name, uint32_t dimension, std::optional<std::string> metric,
+  new_index(std::string name, uint32_t dimension, std::optional<metric_type> metric,
             std::optional<uint16_t> pods, std::optional<pod_configuration> pod_config,
             std::optional<uint16_t> shards, std::optional<uint16_t> replicas,
             std::optional<std::vector<std::string>> metadata_config,
             std::optional<std::string> source_collection) noexcept
       : _name(std::move(name)),
         _dimension(dimension),
-        _metric(std::move(metric)),
+        _metric(metric),
         _pods(pods),
         _pod_config(pod_config),
         _shards(shards),
