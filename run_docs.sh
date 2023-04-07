d60900a2a5c7959ce57a9f754faad4de0a2ef8df
Jordan Kaye <jordan.kaye2@gmail.com>
2023-04-07 17:22:54 +0000

    Pages testing

--- run_docs.sh
+++ run_docs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# TODO: set -eo pipefail
-
 SCRIPTPATH="$(dirname "$0")"
 cd "$SCRIPTPATH" || exit 255
 
 SHOULD_DOXYGEN=1
+SHOULD_BUILD=0
 
 function watch_doxygen() {
     while inotifywait -r -e modify include; do
@@ -20,17 +19,29 @@ while (("$#")); do
         echo "Options:"
         echo "  -h, --help              Show this help message and exit."
         echo "  -sd, --skip-doxygen     Do not generate Doxygen documentation."
+        echo "  -b, --build             Build artifacts rather than serving."
         ;;
     -sd | --skip-doxygen)
         SHOULD_DOXYGEN=0
+        ;;
+    -b | --build)
+        SHOULD_BUILD=1
         ;;
     *) break ;;
     esac
     shift
 done
 
-if [ $SHOULD_DOXYGEN -eq 1 ]; then
+if [ $SHOULD_BUILD -eq 1 ]; then
+    doxygen Doxyfile
+elif [ $SHOULD_DOXYGEN -eq 1 ]; then
     doxygen Doxyfile
     watch_doxygen &
 fi
-mkdocs serve -a 0.0.0.0:8000
+
+if [ $SHOULD_BUILD -eq 1 ]; then
+    cp -R doxygen docs/
+    mkdocs build
+else
+    mkdocs serve -a 0.0.0.0:8000
+fi
